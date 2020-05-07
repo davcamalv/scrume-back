d7e43011a4b3b42edd75d5952557c6d702341f99
davcamalv <dcamalv@gmail.com>
2020-05-07 23:54:32 +0200

    feat, fix(discountCode, travis.yml): añadido modelo  de discountCode
    Cambiado fichero travis para el nuevo despliegue

---
+++ src/main/java/com/spring/Model/DiscountCode.java
@@ -0,0 +1,41 @@
+package com.spring.Model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.SafeHtml;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@EqualsAndHashCode(callSuper = true)
+public class DiscountCode extends BaseEntity {
+	
+	@NotNull
+	@JsonProperty
+	@Column(name = "expiredDate")
+	private Date expiredDate;
+	
+	@JsonProperty
+	@NotBlank
+	@SafeHtml
+	private String code;
+
+}
